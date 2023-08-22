bcf7ff127e7e17ea9d77ffb8f8dfeda9e816e2fe
Bombillo2015 <juan.reyes@davivienda.com>
2023-08-22 11:25:49 -0500

    estructura web finalizada

--- js/main.js
+++ js/main.js
@@ -114,6 +114,7 @@ $(document).ready(function(){
                         infel.classList.add("active");
                     }else{
                         infel.classList.remove("active");
+                        
                     } 
                 });
             }); 
